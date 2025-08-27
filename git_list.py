21a33bd53163997a46376890c2b73d6426d16b37
Mohamed-Aslam0 <mohamedaslam2414@gmail.com>
2025-08-27 23:57:40 +0530

    fit:customer salary

---
+++ git_list.py
@@ -0,0 +1,9 @@
+rows = [
+    ["id", "name", "salary"],
+    [1, "Alice", 50000],
+    [2, "Bob", 60000],
+    [3, "Charlie", 70000]
+]
+
+high_salary = [row[1] for row in rows[1:] if row[2] > 50000]
+print(high_salary)
