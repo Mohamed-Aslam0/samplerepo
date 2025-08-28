3cdc1543266b74bc826c5b83ace9540039514417
Mohamed Aslam <mohamedaslam2414@gmail.com>
2025-08-28 17:12:29 +0530

    Fit: adding name

--- git_list.py
+++ git_list.py
@@ -2,7 +2,8 @@ rows = [
     ["id", "name", "salary"],
     [1, "Alice", 50000],
     [2, "Bob", 60000],
-    [3, "Charlie", 70000]
+    [3, "Charlie", 70000],
+    [4,"Aslam",1000000]
 ]
 
 high_salary = [row[1] for row in rows[1:] if row[2] > 50000]
